b3fa4933e496cada6d195ae272b5180858ede485
VictorH1-oficial <biaribs06@gmail.com>
2025-12-02 19:45:28 -0300

    Change multiplicar to use iterative multiplication
    Refactor multiplicar function to use loops for multiplication.

--- multiplicar.c
+++ multiplicar.c
@@ -1,4 +1,20 @@
+#include <stdio.h>
 
-int multiplicar(int a, int b) {
-    return a * b;
+int multiplicar(numero1,numero2)
+{
+    int contador1,contador2,produto;
+
+    contador1 = 0;
+    contador2 = 0;
+    produto = 0;
+
+    while (contador1 < numero1){
+        contador2 = 0;
+        while (contador2 < numero2){
+            produto++; // produto = produto + 1;
+            contador2++;   
+        }
+        contador1++;    
+    }
+   return produto;
 }
